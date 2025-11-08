dacb6c70deaa935a42b171d924b197bc0cee7d8f
sam88x <seb88x@umsystem.edu>
2025-11-08 16:15:18 -0600

    update position with getter/setter

--- Space-Networking/src/lib/simulator/definitions/position.ts
+++ Space-Networking/src/lib/simulator/definitions/position.ts
@@ -1,10 +1,24 @@
 class Position {
-    x: number;
-    y: number;
+    _x: number;
+    _y: number;
 
     constructor (x: number, y:number) {
-        this.x = x;
-        this.y = y;
+        this._x = x;
+        this._y = y;
+    }
+
+    get x() {
+        return this._x;
+    }
+    set x(value: number) {
+        this._x = value;
+    }
+
+    get y() {
+        return this._y;
+    }
+    set y(value: number) {
+        this._y = value;
     }
 }
 
