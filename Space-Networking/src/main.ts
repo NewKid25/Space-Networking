47cae85005358f5ce182420f4e75d3a68a7a193d
sam88x <seb88x@umsystem.edu>
2025-11-08 19:27:58 -0600

    test main

--- Space-Networking/src/main.ts
+++ Space-Networking/src/main.ts
@@ -7,5 +7,10 @@ import KineticSim from './lib/simulator/kinetic_simulator'
 import Position from './lib/simulator/definitions/position'
 import { DISTANCE_FROM_SUN } from './lib/simulator/constants'
 
-createApp(App).mount('#app')
-// let two_bodies = [new SpaceBody(1, "Sun", [new Position(0, 0)]), new Oribiter(2, "Earth", DISTANCE_FROM_SUN.get("Earth"), )]
+// createApp(App).mount('#app')
+let sun = new SpaceBody(1, "Sun", [new Position(0, 0)])
+let earth = new Oribiter(2, "Earth", 1000000, "Sun", sun, new Position(1000000, 0))
+let satellite = new Oribiter(3, "DA MOOOOON", 80000, "Earth", earth, new Position(0, 1080000))
+
+let two_bodies = [sun, earth, satellite]
+let kSim = new KineticSim(two_bodies, 1000)
